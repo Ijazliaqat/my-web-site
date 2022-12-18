27ae0e0c2a4280e5869947c806c3963087f4f0cd
Ijaz Liaqat <ijazierpu@gmail.com>
2022-12-18 15:51:34 +0500

    changes in mathSolution file

--- mathSolution.js
+++ mathSolution.js
@@ -3,26 +3,21 @@ let inputValue = document.querySelector('.palindrome-input').value;
 let btnPalindrome = document.querySelector('.btn-palindrome');
 let result = document.querySelector('.show-result');
 
-function checkPalindrome(inputValue) {
 
-    const arrayValues = inputValue.split('');
-    const reverseArrayValues = arrayValues.reverse();
-    const reverseString = reverseArrayValues.join('');
-
-    if(inputValue == reverseString) {
-        result.textContent = 'It is a palindrome';
-    }
-    else {
-        result.textContent = 'It is not a palindrome';
-    }
-}
+function checkPalindrome(str) {  
+    const length = str.lengthgth;  
+  
+    for (let i = 0; i < length / 2; i++) {  
+        if (str[i] !== str[length - 1 - i]) {  
+          result.textContent= 'It is not palindrome'; 
+          return;
+        }  
+    }  
+    result.textContent='It is a palindrome';  
+}  
 
 
 btnPalindrome.addEventListener('click', function () {
-    console.log(inputValue);
-    const value = checkPalindrome(inputValue);
-
-    console.log(value);
-    
-})
+    checkPalindrome(inputValue);
+});
 
